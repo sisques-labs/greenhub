7ce3fe7a5180baa6c17417b54c67c6e7362c0069
JSisques <javierplazasisques@gmail.com>
2026-02-21 12:33:02 +0100

    refactor(api): rename PlantSpeciesApplicationNotFoundException to PlantSpeciesNotFoundException
    Updated the exception class name for clarity and consistency. Adjusted the usage in the AssertPlantSpeciesExistsService to reflect the new naming convention.

--- apps/api/src/core/plant-species-context/application/exceptions/plant-species/plant-species-not-found/plant-species-not-found.exception.ts
+++ apps/api/src/core/plant-species-context/application/exceptions/plant-species/plant-species-not-found/plant-species-not-found.exception.ts
@@ -1,6 +1,6 @@
 import { BaseDomainException } from '@/shared/domain/exceptions/base-domain.exception';
 
-export class PlantSpeciesApplicationNotFoundException extends BaseDomainException {
+export class PlantSpeciesNotFoundException extends BaseDomainException {
 	constructor(id: string) {
 		super(`Plant species with id ${id} not found`);
 	}
